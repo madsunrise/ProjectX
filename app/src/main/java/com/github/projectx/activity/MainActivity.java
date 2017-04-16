616e0fec0786a6c18b4fcb2dfd99c707a7f6c9e3
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-04-17 01:58:05 +0300

    Getting service list logic completely moved to fragment

--- app/src/main/java/com/github/projectx/activity/MainActivity.java
+++ app/src/main/java/com/github/projectx/activity/MainActivity.java
@@ -6,19 +6,13 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.github.projectx.R;
 import com.github.projectx.fragment.FeedFragment;
-import com.github.projectx.model.Service;
-import com.github.projectx.network.ServiceController;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,51 +21,24 @@ import static com.github.projectx.utils.Constants.NAV_DRAWER_ARTISTS_SEARCH;
 import static com.github.projectx.utils.Constants.NAV_DRAWER_MESSAGES;
 import static com.github.projectx.utils.Constants.NAV_DRAWER_SETTINGS;
 
-public class MainActivity extends AppCompatActivity implements ServiceController.ServiceListCallback {
+
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     private Drawer drawer;
     private FeedFragment feedFragment = new FeedFragment();
-    private ServiceController serviceController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        serviceController = ServiceController.getInstance(getApplicationContext());
-        serviceController.setServiceListCallback(this);
         ButterKnife.bind(this);
-
         setUpNavDrawer();
-
         changeFragment(feedFragment);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        serviceController.queryForServiceList(null, null, 1, 15);
-    }
-
-    @Override
-    public void onDataLoaded(List<Service> services) {
-        Log.d(TAG, "Service list has been loaded");
-        feedFragment.updateDataSet(services);
-    }
-
-    @Override
-    public void dataLoadingFailed() {
-        Log.d(TAG, "Loading service list from network failed!");
-        Toast.makeText(this, R.string.error_occured, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        serviceController.setServiceListCallback(null);
-    }
 
     private void changeFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
--- app/src/main/java/com/github/projectx/fragment/FeedFragment.java
+++ app/src/main/java/com/github/projectx/fragment/FeedFragment.java
@@ -1,33 +1,38 @@
 package com.github.projectx.fragment;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.github.projectx.R;
 import com.github.projectx.ServiceListAdapter;
 import com.github.projectx.model.Service;
+import com.github.projectx.network.ServiceController;
 
 import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 /**
  * Created by ivan on 16.04.17.
  */
 
-public class FeedFragment extends Fragment {
+public class FeedFragment extends Fragment implements ServiceController.ServiceListCallback {
 
+    private ServiceController serviceController;
     private RecyclerView recyclerView;
     private ServiceListAdapter adapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        serviceController = ServiceController.getInstance(getContext());
+    }
 
     @Nullable
     @Override
@@ -35,8 +40,27 @@ public class FeedFragment extends Fragment {
         View view = inflater.inflate(R.layout.feed_fragment, container, false);
         recyclerView = (RecyclerView) view.findViewById(R.id.service_list_recycler);
         setUpRecyclerView();
+        serviceController.setServiceListCallback(this);
+        serviceController.queryForServiceList(null, null, 1, 20);
         return view;
     }
+
+
+
+    @Override
+    public void onDataLoaded(List<Service> services) {
+        Log.d(TAG, "Service list has been loaded");
+        adapter = new ServiceListAdapter(getContext(), services);
+        recyclerView.swapAdapter(adapter, false);
+    }
+
+
+    @Override
+    public void dataLoadingFailed() {
+        Log.d(TAG, "Loading service list from network failed!");
+        Toast.makeText(getContext(), R.string.error_occured, Toast.LENGTH_SHORT).show();
+    }
+
 
     private void setUpRecyclerView() {
         LinearLayoutManager llm = new LinearLayoutManager(getContext());
@@ -45,8 +69,11 @@ public class FeedFragment extends Fragment {
         recyclerView.setHasFixedSize(true);
     }
 
-    public void updateDataSet(List<Service> services) {
-        adapter = new ServiceListAdapter(getContext(), services);
-        recyclerView.swapAdapter(adapter, false);
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        serviceController.setServiceListCallback(null);
     }
+
+    private static final String TAG = FeedFragment.class.getSimpleName();
 }
